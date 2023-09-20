baddc1131fb37c0a675fdca1ca3bc1d0db41f7fe
John Doe <johndoe@example.com>
2023-09-19 20:39:39 -0700

    new

---
+++ projects/build14/lib_src/CMakeFiles/l.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/l.dir/src/l.cpp.o"
+  "../lib/libl.pdb"
+  "../lib/libl.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/l.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
