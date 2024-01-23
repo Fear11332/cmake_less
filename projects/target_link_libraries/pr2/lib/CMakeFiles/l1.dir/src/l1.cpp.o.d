5b9d28aa8532c396fa41170313f8a0232e7a8613
Your Name <you@example.com>
2024-01-23 20:09:30 +0400

    new

---
+++ projects/target_link_libraries/pr2/lib/CMakeFiles/l1.dir/src/l1.cpp.o.d
@@ -0,0 +1,4 @@
+lib/CMakeFiles/l1.dir/src/l1.cpp.o: \
+ /mnt/44580EA6580E9738/Users/saw/Desktop/Users/saw/Desktop/Users/saw/Desktop/study/buid_system_cmake/projects/target_link_libraries/pr2/lib/src/l1.cpp \
+ /usr/include/stdc-predef.h \
+ /mnt/44580EA6580E9738/Users/saw/Desktop/Users/saw/Desktop/Users/saw/Desktop/study/buid_system_cmake/projects/target_link_libraries/pr2/lib/include/l1.h
---
+++ projects/target_link_libraries/pr2/src/main.cpp
@@ -0,0 +1,9 @@
+#include "l1.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<<f()<<endl;
+}
