5cb6f5e197ce9eb98f434e9defe93f07e3f559e9
fear <gerasimovsawwa@gmail.com>
2024-03-09 19:17:40 +0400

    new

---
+++ projects/generator_expressions/bool_expr/CMakeFiles/bool_expr.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bool_expr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/bool_expr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ projects/generator_expressions/bool_expr/CMakeFiles/bool_expr.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for bool_expr.
+# This may be replaced when dependencies are built.
